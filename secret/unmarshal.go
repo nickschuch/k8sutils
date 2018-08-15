340eeddea9a86dded1743bfacf9cb8cdbf816d53
Nick Schuch <nick@myschuch.com>
2018-08-15 13:28:06 +1000

    Secret: Fix base64

--- secret/unmarshal.go
+++ secret/unmarshal.go
@@ -1,8 +1,6 @@
 package secret
 
 import (
-	"encoding/base64"
-	"fmt"
 	"reflect"
 
 	corev1 "k8s.io/api/core/v1"
@@ -28,11 +26,7 @@ func Unmarshal(s *corev1.Secret, obj interface{}) error {
 
 		if d, ok := s.Data[tag]; ok {
 			if val.Field(i).Kind() == reflect.String {
-				dec, err := base64.StdEncoding.DecodeString(string(d))
-				if err != nil {
-					return fmt.Errorf("failed to unmarshal %s with value %s to type string: %s", tag, d, err)
-				}
-				val.Field(i).SetString(string(dec))
+				val.Field(i).SetString(string(d))
 			}
 		}
 	}
